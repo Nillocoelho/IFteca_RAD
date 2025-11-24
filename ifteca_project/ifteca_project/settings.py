faa24f424174259120a25600a7f5ce91437a9990
Danillo Coelho Barbosa <coelho.danillo@academico.ifpb.edu.br>
2025-11-24 17:10:26 -0300

    chore(reorg): move sqlite DB to data/ and update settings

--- ifteca_project/ifteca_project/settings.py
+++ ifteca_project/ifteca_project/settings.py
@@ -80,7 +80,8 @@ WSGI_APPLICATION = 'ifteca_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # Database moved to repository-root `data/` for consistency
+        'NAME': BASE_DIR.parent / 'data' / 'db.sqlite3',
     }
 }
 
